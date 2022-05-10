0196fd8a4feb9bd746feddb3a23aa5cb8883bed2
Giorgi <Gio.beridze33@gmail.com>
2022-05-10 13:55:20 +0400

    added bottom navigation view with menu

--- app/src/main/java/com/example/footballapp/MainActivity.kt
+++ app/src/main/java/com/example/footballapp/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.footballapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.footballapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setUpBottomNavView()
+    }
+
+    private fun setUpBottomNavView() {
+        val navigationHost =
+            supportFragmentManager.findFragmentById(R.id.mainNavHostFragment) as NavHostFragment
+        val navController = navigationHost.navController
+        binding.bottomNavView.setupWithNavController(navController)
     }
 }
