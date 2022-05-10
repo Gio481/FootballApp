cec47823f6d1a131ce2d575718a1d498af24f1a4
Giorgi <Gio.beridze33@gmail.com>
2022-05-10 15:51:13 +0400

    create retrofit instance

---
+++ app/src/main/java/com/example/footballapp/data/network/RetrofitInstance.kt
@@ -0,0 +1,31 @@
+package com.example.footballapp.data.network
+
+import com.example.footballapp.data.network.interceptor.NetworkInterceptor
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+private const val BASE_URL = "http://www.mocky.io/"
+
+fun provideUserApi(retrofit: Retrofit): ApiService {
+    return retrofit.create(ApiService::class.java)
+}
+
+fun provideGsonConvertorFactory(): GsonConverterFactory = GsonConverterFactory.create()
+
+fun provideHttpClient(): OkHttpClient {
+    return OkHttpClient.Builder()
+        .addInterceptor(NetworkInterceptor()).build()
+}
+
+fun provideRetrofit(): Retrofit {
+    return retrofit {
+        baseUrl(BASE_URL)
+        client(provideHttpClient())
+        addConverterFactory(provideGsonConvertorFactory())
+    }
+}
+
+fun retrofit(block: Retrofit.Builder.() -> Unit): Retrofit {
+    return Retrofit.Builder().apply(block).build()
+}
