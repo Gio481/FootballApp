0d1741cd36a7b75ddb41a9b4f9c29f127a19f5cc
Giorgi <Gio.beridze33@gmail.com>
2022-05-10 13:52:27 +0400

    create match overview custom view

---
+++ app/src/main/java/com/example/footballapp/presentation/custom_view/MatchOverviewCustomView.kt
@@ -0,0 +1,19 @@
+package com.example.footballapp.presentation.custom_view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.FrameLayout
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.footballapp.R
+import com.example.footballapp.databinding.MatchOverviewCustomViewBinding
+
+class MatchOverviewCustomView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet,
+    defStyleAttr: Int = 0,
+) : FrameLayout(context, attrs, defStyleAttr) {
+
+    private val binding: MatchOverviewCustomViewBinding =
+        MatchOverviewCustomViewBinding.inflate(LayoutInflater.from(context), this, true)
+}
