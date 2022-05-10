fb6209095844deb803943f5262b1a989ecf3439f
Giorgi <Gio.beridze33@gmail.com>
2022-05-10 13:53:12 +0400

    create clubs info custom view

---
+++ app/src/main/java/com/example/footballapp/presentation/custom_view/ClubsInfoCustomView.kt
@@ -0,0 +1,60 @@
+package com.example.footballapp.presentation.custom_view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.footballapp.R
+import com.example.footballapp.databinding.ClubsInfoCustomViewBinding
+
+class ClubsInfoCustomView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet,
+    defStyleAttr: Int = 0,
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+    private val binding: ClubsInfoCustomViewBinding by lazy {
+        ClubsInfoCustomViewBinding.inflate(LayoutInflater.from(context), this, true)
+    }
+
+    var homeTeamName: String? = null
+        set(value) {
+            binding.homeClubNameTextView.text = value
+            field = value
+        }
+
+    var awayTeamName: String? = null
+        set(value) {
+            binding.awayClubNameTextView.text = value
+            field = value
+        }
+
+    var score: String? = null
+        set(value) {
+            binding.scoreTextView.text = value
+            field = value
+        }
+
+    var time: String? = null
+        set(value) {
+            binding.matchTimeTextView.text = value
+            field = value
+        }
+
+    init {
+        val typedArray = context.theme.obtainStyledAttributes(
+            attrs,
+            R.styleable.ClubsInfoCustomView,
+            defStyleAttr,
+            0
+        )
+        homeTeamName = typedArray.getString(R.styleable.ClubsInfoCustomView_homeTeamName)
+        awayTeamName = typedArray.getString(R.styleable.ClubsInfoCustomView_awayTeamName)
+        score = typedArray.getString(R.styleable.ClubsInfoCustomView_score)
+        time = typedArray.getString(R.styleable.ClubsInfoCustomView_time)
+        typedArray.recycle()
+    }
+
+    fun setHomeTeamLogo(url: String) {}
+
+    fun setAwayTeamLogo(url: String) {}
+}
