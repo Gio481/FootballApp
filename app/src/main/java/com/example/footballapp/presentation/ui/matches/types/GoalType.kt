f20dbb2ca87c40288d685300a06805c75fed97b6
Giorgi <Gio.beridze33@gmail.com>
2022-05-17 02:23:40 +0400

    added enums to determine which type is happened

---
+++ app/src/main/java/com/example/footballapp/presentation/ui/matches/types/GoalType.kt
@@ -0,0 +1,5 @@
+package com.example.footballapp.presentation.ui.matches.types
+
+enum class GoalType(val value: Int) {
+    GOAL(1), OWN_GOAL(2)
+}
---
+++ app/src/main/java/com/example/footballapp/presentation/ui/matches/types/MatchTeamType.kt
@@ -0,0 +1,5 @@
+package com.example.footballapp.presentation.ui.matches.types
+
+enum class MatchTeamType(val value: Int) {
+    TEAM1(1), TEAM2(2)
+}
