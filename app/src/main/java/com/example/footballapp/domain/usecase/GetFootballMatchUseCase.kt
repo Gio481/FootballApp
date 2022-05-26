530bd9f84766bfa42f960695b9ad00034ab82224
Giorgi <Gio.beridze33@gmail.com>
2022-05-26 11:42:20 +0400

    added functions to count first and second half scores

--- app/src/main/java/com/example/footballapp/domain/usecase/GetFootballMatchUseCase.kt
+++ app/src/main/java/com/example/footballapp/domain/usecase/GetFootballMatchUseCase.kt
@@ -4,4 +4,6 @@ import com.example.footballapp.domain.model.MatchDomain
 
 interface GetFootballMatchUseCase {
     suspend fun getFootballMatch(action: (message: String) -> Unit): MatchDomain?
+    suspend fun getFirstHalfScore(action: (message: String) -> Unit): String
+    suspend fun getSecondHalfScore(action: (message: String) -> Unit): String
 }
--- app/src/main/java/com/example/footballapp/domain/usecase/GetFootballMatchUseCaseImpl.kt
+++ app/src/main/java/com/example/footballapp/domain/usecase/GetFootballMatchUseCaseImpl.kt
@@ -1,13 +1,59 @@
 package com.example.footballapp.domain.usecase
 
 import com.example.footballapp.domain.model.MatchDomain
+import com.example.footballapp.domain.model.TeamActionDomain
 import com.example.footballapp.domain.repository.FootballMatchRepository
+import com.example.footballapp.presentation.match.types.GoalType
+import com.example.footballapp.presentation.match.types.MatchTeamType
 import com.example.footballapp.util.repositoryDataFetcher
 
 class GetFootballMatchUseCaseImpl(private val repository: FootballMatchRepository) :
     GetFootballMatchUseCase {
 
+    private var firstTeamScore=0
+    private var secondTeamScore=0
     override suspend fun getFootballMatch(action: (message: String) -> Unit): MatchDomain? {
         return repositoryDataFetcher({ repository.footballMatch() }, { action(it) })
+    }
+
+
+    override suspend fun getFirstHalfScore(action: (message: String) -> Unit): String {
+        makeTeamsScoresNull()
+        getFootballMatch(action)?.summary?.map {
+            it.team1Action?.map { action -> countGoals(action, it.actionTime, 0..45) }
+            it.team2Action?.map { action -> countGoals(action, it.actionTime, 0..45) }
+        }
+        return "$firstTeamScore : $secondTeamScore"
+    }
+
+    override suspend fun getSecondHalfScore(action: (message: String) -> Unit): String {
+        makeTeamsScoresNull()
+        getFootballMatch(action)?.summary?.map {
+            it.team1Action?.map { action -> countGoals(action, it.actionTime, 46..90) }
+            it.team2Action?.map { action -> countGoals(action, it.actionTime, 46..90) }
+        }
+        return "$firstTeamScore : $secondTeamScore"
+    }
+
+    private fun countGoals(team: TeamActionDomain, actionTime: String, time: IntRange) {
+        val secondTeam = team.teamType == MatchTeamType.TEAM2.value
+        val firstTeam = team.teamType == MatchTeamType.TEAM1.value
+        val ownGoal = team.action.goalType == GoalType.OWN_GOAL.value
+        val goal = team.action.goalType == GoalType.GOAL.value
+
+        if (actionTime.toInt() in time && secondTeam && goal) {
+            secondTeamScore++
+        } else if (actionTime.toInt() in time && secondTeam && ownGoal) {
+            firstTeamScore++
+        } else if (actionTime.toInt() in time && firstTeam && goal) {
+            firstTeamScore++
+        } else if (actionTime.toInt() in time && firstTeam && ownGoal) {
+            secondTeamScore++
+        }
+    }
+
+    private fun makeTeamsScoresNull() {
+        firstTeamScore = 0
+        secondTeamScore = 0
     }
 }
