e8108547b646c06f46de67b89abc193f92e3fea7
Giorgi <Gio.beridze33@gmail.com>
2022-05-26 11:43:23 +0400

    configure match view model

--- app/src/main/java/com/example/footballapp/presentation/match/viewmodel/MatchesViewModel.kt
+++ app/src/main/java/com/example/footballapp/presentation/match/viewmodel/MatchesViewModel.kt
@@ -1,6 +1,40 @@
 package com.example.footballapp.presentation.match.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.footballapp.domain.model.MatchDomain
+import com.example.footballapp.util.KoinComponentInstances
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class MatchesViewModel: ViewModel() {
+class MatchesViewModel : ViewModel() {
+    private val koinComponents by lazy { KoinComponentInstances() }
+
+    private val _footballMatchLiveData: MutableLiveData<MatchDomain> = MutableLiveData()
+    val footballMatchLiveData: LiveData<MatchDomain> = _footballMatchLiveData
+
+    private val _errorLiveData: MutableLiveData<String> = MutableLiveData()
+    val errorLiveData: LiveData<String> = _errorLiveData
+
+    fun getFootballMatch() {
+        viewModelScope.launch(Dispatchers.IO) {
+            _footballMatchLiveData.postValue(koinComponents.matchUseCase.getFootballMatch {
+                _errorLiveData.postValue(it)
+            })
+        }
+    }
+
+    suspend fun firstHalfScore(): String {
+        return koinComponents.matchUseCase.getFirstHalfScore {
+            _errorLiveData.postValue(it)
+        }
+    }
+
+    suspend fun secondHalfScore(): String {
+        return koinComponents.matchUseCase.getSecondHalfScore {
+            _errorLiveData.postValue(it)
+        }
+    }
 }
