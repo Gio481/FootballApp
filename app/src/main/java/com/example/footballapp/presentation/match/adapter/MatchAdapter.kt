a8614bd0535771c1fd03f5cd2e1c086613fc09ec
Giorgi <Gio.beridze33@gmail.com>
2022-05-31 01:43:42 +0400

    added some variables and functions

--- app/src/main/java/com/example/footballapp/presentation/match/adapter/MatchAdapter.kt
+++ app/src/main/java/com/example/footballapp/presentation/match/adapter/MatchAdapter.kt
@@ -9,7 +9,11 @@ import com.example.footballapp.domain.model.SummaryDomain
 import com.example.footballapp.presentation.match.adapter.helper.AdapterHelper
 import com.example.footballapp.util.ItemsDiffUtil
 
-class MatchAdapter(private val helper: AdapterHelper) : ListAdapter<SummaryDomain, MatchAdapter.ViewHolder>(ItemsDiffUtil()) {
+class MatchAdapter(private val helper: AdapterHelper) :
+    ListAdapter<SummaryDomain, MatchAdapter.ViewHolder>(ItemsDiffUtil()) {
+
+    private lateinit var firsHalfSore: String
+    private lateinit var secondHalfScore: String
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(MatchActionLayoutBinding.inflate(LayoutInflater.from(parent.context),
@@ -17,18 +21,25 @@ class MatchAdapter(private val helper: AdapterHelper) : ListAdapter<SummaryDomai
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.onBind(getItem(position), helper)
+        holder.onBind(getItem(position), helper, firsHalfSore, secondHalfScore)
     }
 
-    class ViewHolder(private val binding: MatchActionLayoutBinding, ) : RecyclerView.ViewHolder(binding.root) {
-        fun onBind(summaryDomain: SummaryDomain, helper: AdapterHelper) {
+
+    class ViewHolder(private val binding: MatchActionLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun onBind(summaryDomain: SummaryDomain, helper: AdapterHelper, firsHalf: String, secondHalf: String) {
             with(binding) {
                 with(summaryDomain) {
-                    helper.determineHalfTime(halfTimeCustomView, summaryDomain)
-                    team1Action?.let { team1ActionsContainerCustomView.getFootballTeamInfo(it, actionTime) }
-                    team2Action?.let { team2ActionsContainerCustomView.getFootballTeamInfo(it, actionTime) }
+                    helper.determineHalfTime(halfScoreCustomView, summaryDomain, firsHalf, secondHalf)
+                    team1Action?.let { team1ActionsContainerCustomView.getFootballTeamInfo(helper.domainMapper(it), actionTime) }
+                    team2Action?.let { team2ActionsContainerCustomView.getFootballTeamInfo(helper.domainMapper(it), actionTime) }
                 }
             }
         }
+    }
+
+    fun setHalfTimeScore(firsHalf: String, secondHalf: String) {
+        this.firsHalfSore = firsHalf
+        this.secondHalfScore = secondHalf
     }
 }
