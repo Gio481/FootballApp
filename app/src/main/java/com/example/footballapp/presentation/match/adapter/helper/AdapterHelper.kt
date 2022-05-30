61049fd857a0efb99ee74aaedd579be770d7fadc
Giorgi <Gio.beridze33@gmail.com>
2022-05-31 01:36:41 +0400

    added domain mapper function in adapter helper class

--- app/src/main/java/com/example/footballapp/presentation/match/adapter/helper/AdapterHelper.kt
+++ app/src/main/java/com/example/footballapp/presentation/match/adapter/helper/AdapterHelper.kt
@@ -1,8 +1,11 @@
 package com.example.footballapp.presentation.match.adapter.helper
 
+import com.example.customview.model.TeamActionUI
+import com.example.customview.ui.HalfTimeCustomView
 import com.example.footballapp.domain.model.SummaryDomain
-import com.example.footballapp.presentation.match.custom_view.HalfTimeCustomView
+import com.example.footballapp.domain.model.TeamActionDomain
 
 interface AdapterHelper {
-    fun determineHalfTime(view: HalfTimeCustomView, summaryDomain: SummaryDomain)
+    fun determineHalfTime(view: HalfTimeCustomView, summaryDomain: SummaryDomain,firstHalf:String, secondHalf:String)
+    fun domainMapper(list: List<TeamActionDomain>): List<TeamActionUI>
 }
--- app/src/main/java/com/example/footballapp/presentation/match/adapter/helper/AdapterHelperImpl.kt
+++ app/src/main/java/com/example/footballapp/presentation/match/adapter/helper/AdapterHelperImpl.kt
@@ -1,33 +1,52 @@
 package com.example.footballapp.presentation.match.adapter.helper
 
+import com.example.customview.extension.isVisible
+import com.example.customview.model.TeamActionUI
+import com.example.customview.types.HalfTimeType
+import com.example.customview.ui.HalfTimeCustomView
+import com.example.footballapp.domain.mapper.TeamActionDomainMapper
 import com.example.footballapp.domain.model.SummaryDomain
-import com.example.footballapp.presentation.match.custom_view.HalfTimeCustomView
-import com.example.footballapp.presentation.match.types.HalfTimeType
-import com.example.footballapp.util.extensions.isVisible
+import com.example.footballapp.domain.model.TeamActionDomain
 
-class AdapterHelperImpl(private val firsHalfScore:String, private val secondHalfScore:String):AdapterHelper {
+class AdapterHelperImpl(
+    private val mapper: TeamActionDomainMapper,
+) : AdapterHelper {
 
     private var isFirstHalfChecked = false
     private var isSecondHalfChecked = false
 
-    override fun determineHalfTime(view: HalfTimeCustomView, summaryDomain: SummaryDomain) {
+    override fun determineHalfTime(
+        view: HalfTimeCustomView,
+        summaryDomain: SummaryDomain,
+        firstHalf: String,
+        secondHalf: String,
+    ) {
         if (summaryDomain.actionTime.toInt() < FIRST_HALF_TIME_MINUTES && !isFirstHalfChecked) {
             isFirstHalfChecked = true
-            showHalfTimeView(view, HalfTimeType.FirstHalf, firsHalfScore)
+            showHalfTimeView(view, HalfTimeType.FirstHalf, firstHalf)
         } else if (summaryDomain.actionTime.toInt() > FIRST_HALF_TIME_MINUTES && !isSecondHalfChecked) {
             isSecondHalfChecked = true
-            showHalfTimeView(view, HalfTimeType.SecondHalf, secondHalfScore)
+            showHalfTimeView(view, HalfTimeType.SecondHalf, secondHalf)
         } else {
+
             view.isVisible(false)
         }
     }
 
-    private fun showHalfTimeView(view: HalfTimeCustomView, halfTimeType: HalfTimeType, halfTimeScore:String) {
+    private fun showHalfTimeView(
+        view: HalfTimeCustomView,
+        halfTimeType: HalfTimeType,
+        halfScore: String,
+    ) {
         with(view) {
             halfTime = halfTimeType
-            score = halfTimeScore
+            score = halfScore
             isVisible(true)
         }
+    }
+
+    override fun domainMapper(list: List<TeamActionDomain>): List<TeamActionUI> {
+        return mapper.modelListMapper(list)
     }
 
     companion object {
