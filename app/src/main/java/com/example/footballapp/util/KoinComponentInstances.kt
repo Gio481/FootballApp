274e0b876b57f5c2b6f92466585010de7bec1a15
Giorgi <Gio.beridze33@gmail.com>
2022-05-31 14:54:03 +0400

    added half score counter use case in koin component instance

--- app/src/main/java/com/example/footballapp/util/KoinComponentInstances.kt
+++ app/src/main/java/com/example/footballapp/util/KoinComponentInstances.kt
@@ -1,9 +1,11 @@
 package com.example.footballapp.util
 
-import com.example.footballapp.domain.usecase.GetFootballMatchUseCase
+import com.example.footballapp.domain.usecase.get_match.GetFootballMatchUseCase
+import com.example.footballapp.domain.usecase.get_score.GetHalfTimeScoreUseCase
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
-class KoinComponentInstances:KoinComponent {
+class KoinComponentInstances : KoinComponent {
     val matchUseCase by inject<GetFootballMatchUseCase>()
+    val haltTimeScoreUseCase by inject<GetHalfTimeScoreUseCase>()
 }
