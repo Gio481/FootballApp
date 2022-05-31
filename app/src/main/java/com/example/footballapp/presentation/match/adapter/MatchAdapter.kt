ff66c31717db198231d18923abfe30c31eac85dd
Giorgi <Gio.beridze33@gmail.com>
2022-05-31 14:55:19 +0400

    clean up adapter

--- app/src/main/java/com/example/footballapp/presentation/match/adapter/MatchAdapter.kt
+++ app/src/main/java/com/example/footballapp/presentation/match/adapter/MatchAdapter.kt
@@ -12,34 +12,25 @@ import com.example.footballapp.util.ItemsDiffUtil
 class MatchAdapter(private val helper: AdapterHelper) :
     ListAdapter<SummaryDomain, MatchAdapter.ViewHolder>(ItemsDiffUtil()) {
 
-    private lateinit var firsHalfSore: String
-    private lateinit var secondHalfScore: String
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(MatchActionLayoutBinding.inflate(LayoutInflater.from(parent.context),
             parent, false))
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.onBind(getItem(position), helper, firsHalfSore, secondHalfScore)
+        holder.onBind(getItem(position), helper)
     }
-
 
     class ViewHolder(private val binding: MatchActionLayoutBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        fun onBind(summaryDomain: SummaryDomain, helper: AdapterHelper, firsHalf: String, secondHalf: String) {
+        fun onBind(summaryDomain: SummaryDomain, helper: AdapterHelper) {
             with(binding) {
                 with(summaryDomain) {
-                    helper.determineHalfTime(halfScoreCustomView, summaryDomain, firsHalf, secondHalf)
+                    helper.determineHalfTime(halfScoreCustomView, summaryDomain)
                     team1Action?.let { team1ActionsContainerCustomView.getFootballTeamInfo(helper.domainMapper(it), actionTime) }
                     team2Action?.let { team2ActionsContainerCustomView.getFootballTeamInfo(helper.domainMapper(it), actionTime) }
                 }
             }
         }
-    }
-
-    fun setHalfTimeScore(firsHalf: String, secondHalf: String) {
-        this.firsHalfSore = firsHalf
-        this.secondHalfScore = secondHalf
     }
 }
